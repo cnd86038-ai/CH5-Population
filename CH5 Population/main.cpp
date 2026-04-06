8e9544d74a0ecffe952627570d0324451805a7ec
cnd86038-ai <cnd86038@email.vccs.edu>
2026-04-06 14:52:12 -0400

    Step 2. adding the daily increase number as a precent. Used double to allow for decimals. I also used while (dailyincrease <= 0) to ensure that any number equal to or less than 0 when plugged in will result in a failure.

--- CH5 Population/main.cpp
+++ CH5 Population/main.cpp
@@ -17,4 +17,14 @@ int main()
 		cout << "Starting population must be at least 2. Please re-enter: ";
 		cin >> startingpopulation;
 	}
+
+	// Get daily increase %
+	cout << "Enter the daily population increase (as a percentage): ";
+	cin >> dailyincrease;
+
+	while (dailyincrease <= 0)
+	{
+		cout << "Daily increase must be a positive number. Please re-enter: ";
+		cin >> dailyincrease;
+	}
 }
